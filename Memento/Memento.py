5dbf55e5fe25663fed716a4a22f7699a7d8d9ee7
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2023-04-24 23:58:23 +0900

    Update Memento.py

--- Memento/Memento.py
+++ Memento/Memento.py
@@ -3,3 +3,25 @@ from abc import ABC, abstractmethod
 from datetime import datetime
 from random import sample
 from string import ascii_letters, digits
+
+class Originator():
+    _state = None
+
+    def __init__(self, state: str) -> None:
+        self._state = state
+        print(f"Originator: My initial state is : {self._state}")
+
+    def do_something(self) -> None:
+        print("Originator: I'm doing something important.")
+        self._state = self._generate_random_string(30)
+        print(f"Originator: and my state has changed to: {self._state}")
+
+    def _generate_random_string(self, length: int = 10) -> None:
+        return "".join(sample(ascii_letters, length))
+
+    def save(self) -> Memento:
+         return ConcreteMemento(self._state)
+    
+def restore(self, memento: Memento) -> None:
+    self._state = memento.get_state()
+    print(f"Originator: My state has changed to: {self._state}")
