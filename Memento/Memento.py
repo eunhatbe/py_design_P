502a9bba1299f60cea14d55232f4691297bc707f
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2023-04-25 23:59:02 +0900

    Update Memento.py

--- Memento/Memento.py
+++ Memento/Memento.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from random import sample
 from string import ascii_letters, digits
 
+
 class Originator():
     _state = None
 
@@ -20,8 +21,64 @@ class Originator():
         return "".join(sample(ascii_letters, length))
 
     def save(self) -> Memento:
-         return ConcreteMemento(self._state)
-    
+        return ConcreteMemento(self._state)
+
     def restore(self, memento: Memento) -> None:
         self._state = memento.get_state()
         print(f"Originator: My state has changed to: {self._state}")
+        
+        
+class Memento(ABC):
+
+    @abstractmethod
+    def get_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_date(self) -> str:
+        pass
+
+
+class ConcreteMemento(Memento):
+    
+    def __init__(self, state: str) -> None:
+        self._state = state
+        self._date = str(datetime.now())[:19]
+
+    def get_state(self) -> str:
+        return self._state
+
+    def get_name(self) -> str:
+        return f"{self._date} / ({self._state[0:9]}...)"
+
+    def get_date(self) -> str:
+        return self._date
+
+
+class Caretaker():
+
+    def __init__(self, originator: Originator) -> None:
+        self._mementos = []
+        self._originator = originator
+
+    def backup(self) -> None:
+        print("\nCaretaker: Saving Originator's state...")
+        self._mementos.append(self._originator.save())
+
+    def undo(self) -> None:
+        if not len(self._mementos):
+            return
+
+        memento = self._mementos.pop()
+        print(f"Caretaker: Restoring state to: {memento.get_name()}")
+        try:
+            self._originator.restore(memento)
+        except Exception:
+            self.undo()
+
+    def show_history(self) -> None:
+        print("Caretaker: Here's the list of mementos:")
+        for memento in self._mementos:
+            print(memento.get_name())
+
+
